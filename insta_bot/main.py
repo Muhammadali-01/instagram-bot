3ff2d71fd5530092845ec7fc2e94830b99dcfe70
Muhammadali-01 <126440659+Muhammadali-01@users.noreply.github.com>
2023-05-31 09:49:51 +0500

    Hatolar to'g'irlandi

--- insta_bot/main.py
+++ insta_bot/main.py
@@ -2,7 +2,7 @@ import logging
 
 from aiogram import Bot, Dispatcher, executor, types
 from test import video_yuklab_ber
-API_TOKEN = '5758456770:AAHSCx3HxoqiMhW0LbCG8sMT5qMVXFiaTPo'
+API_TOKEN = 'YOUR API TOKEN'
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -14,7 +14,7 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    await message.reply("Bu insta bot")
+    await message.reply("Salom\n Bu instagram bot")
 
 
 
@@ -23,7 +23,7 @@ async def echo(message: types.Message):
     mes_text = message.text
     video_link = video_yuklab_ber(mes_text)
     await message.answer("Video yuklanmoqda")
-    await message.answer_video(video_link, caption="bu zo'r video. Murojaat ")
+    await message.answer_video(video_link, caption="video ma'lumot")
 
 
 if __name__ == '__main__':
